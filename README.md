cf265935735bd2cc3564f72061e144ccd47a2a5d
David Valencia <dvalencia@drift.com>
2022-11-16 11:54:51 -0500

    readme updates

--- README.md
+++ README.md
@@ -8,16 +8,82 @@ Drift API wrapper for Node.
 npm install drift-npm --save
 ```
 
-## List of available methods
+## API Token
+
+## Contacts API
+
+### `createContact(attributes, token)`
+
+When creating a new contact, you must send an `attributes` object that contains, at least, an `email` key. To view the full list of default attributes, visit [this link](https://devdocs.drift.com/docs/contact-model).
+
+#### Usage:
 
 ```
-createMessage(conversationId, options, token)
+const driftApi =  require("drift-npm");
+const  token  =  "XXXXXXXXXXXXXX"; //Replace with your token;
+const  attributes  =  {
+			email:  "newcontact@email.com",
+			name:  "New Contact",
+};
+driftApi.createContact(attributes,  token).then((res)  =>  console.log(res));
 ```
 
-```
-createContact(data, token)
-```
+#### Result:
 
 ```
-updateContact(contactId, data, token)
+{
+  data: {
+    id: 16344778575,
+    createdAt: 1668615901358,
+    attributes: {
+      _END_USER_VERSION: 3,
+      _end_user_version: 3,
+      _calculated_version: 3,
+      socialProfiles: {},
+      name: 'New Contact',
+      email: 'newcontact@email.com',
+      events: {},
+      tags: [],
+      start_date: 1668615901358
+    }
+  }
+}
+```
+
+### `updateContact(contactId, attributesToUpdate, token)`
+
+You can update a contact record by providing its `contactId` and an object with the attributes to update.
+To create and maintain custom attributes, just perform a PATCH with the new attribute, and we will respect the attribute.
+
+#### Usage:
+
+```
+const driftApi =  require("drift-npm");
+const  token  =  "XXXXXXXXXXXXXX"; //Replace with your token;
+const  attributesToUpdate  =  {
+					name:  "Updated Contact",
+};
+driftApi.updateContact("16344778575",  attributesToUpdate,  token).then((res)  =>  console.log(res));
+```
+
+#### Result:
+
+```
+{
+  data: {
+    id: 16344778575,
+    createdAt: 1668615901358,
+    attributes: {
+      _END_USER_VERSION: 3,
+      _end_user_version: 3,
+      _calculated_version: 3,
+      socialProfiles: {},
+      name: 'Updated Contact',
+      email: 'newcontact@email.com',
+      events: {},
+      tags: [],
+      start_date: 1668615901358
+    }
+  }
+}
 ```
--- index.js
+++ index.js
@@ -28,7 +28,7 @@ async function createContact(data, token) {
 			JSON.stringify({ attributes: data }),
 			{ headers }
 		);
-		return response;
+		return response.data;
 	} catch (error) {
 		console.error(error);
 	}
@@ -45,12 +45,103 @@ async function updateContact(contactId, data, token) {
 			JSON.stringify({ attributes: data }),
 			{ headers }
 		);
+		return response.data;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+async function retrieveContact(contactId, token) {
+	const headers = {
+		Authorization: `Bearer ${token}`,
+		"Content-Type": "application/json",
+	};
+	try {
+		const response = await axios.get(
+			`https://driftapi.com/contacts/${contactId}`,
+			{ headers }
+		);
+		return response.data;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+async function deleteContact(contactId, token) {
+	const headers = {
+		Authorization: `Bearer ${token}`,
+		"Content-Type": "application/json",
+	};
+	try {
+		const response = await axios.delete(
+			`https://driftapi.com/contacts/${contactId}`,
+			{ headers }
+		);
 		return response;
 	} catch (error) {
 		console.error(error);
 	}
 }
 
-module.exports.createMessage = createMessage;
-module.exports.createContact = createContact;
-module.exports.updateContact = updateContact;
+async function unsuscribeContacts(emails, token) {
+	const headers = {
+		Authorization: `Bearer ${token}`,
+		"Content-Type": "application/json",
+	};
+	try {
+		const response = await axios.post(
+			`https://driftapi.com/emails/unsubscribe`,
+			JSON.stringify(emails),
+			{ headers }
+		);
+		return "Contacts unsuscribed succesfully";
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+async function postEvent(data, token) {
+	//Required Scope: contact_write
+	const headers = {
+		Authorization: `Bearer ${token}`,
+		"Content-Type": "application/json",
+	};
+	try {
+		const response = await axios.post(
+			`https://driftapi.com/contacts/timeline`,
+			{ contactId: data.contactId, event: data.event },
+			{ headers }
+		);
+		return response;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+async function listCustomAttributes(token) {
+	//Require Scope: all_read_contacts
+	const headers = {
+		Authorization: `Bearer ${token}`,
+		"Content-Type": "application/json",
+	};
+	try {
+		const response = await axios.get(
+			`https://driftapi.com/contacts/attributes`,
+			{ headers }
+		);
+		return response.data.data;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+module.exports = {
+	createMessage,
+	createContact,
+	updateContact,
+	retrieveContact,
+	deleteContact,
+	unsuscribeContacts,
+	postEvent,
+	listCustomAttributes,
+};
